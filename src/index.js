6cd153cd826608aa8053cd4dd08f0954e57c8f5d
sohee <sohee5143@naver.com>
2024-04-01 22:03:08 +0900

    Fix: 배포 url 수정

--- src/index.js
+++ src/index.js
@@ -6,9 +6,8 @@ import GlobalStyle from "./GlobalStyles";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <GlobalStyle />
-
       <App />
     </BrowserRouter>
   </React.StrictMode>
