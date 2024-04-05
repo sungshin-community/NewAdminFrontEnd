c5c58543e933eab15921aaa3dbdfc74382aea91a
linason-esc <linason7889@gmail.com>
2024-04-05 10:48:57 +0900

    feat: 로그인 로직 수정

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import SideBar from "../components/SideBar";
 import PostMannager from "../components/PostMannager";
 import styled from "styled-components";
@@ -6,32 +7,50 @@ import DetailsView from "../components/DetailsView";
 import Writing from "../components/Writing";
 
 export default function Main() {
-    const [isWriting, setIsWriting] = useState(false);
-    const [selectedPostId, setSelectedPostId] = useState(null); 
-    const [isModifying, setIsModifying] = useState(false);
+  const [isWriting, setIsWriting] = useState(false);
+  const [selectedPostId, setSelectedPostId] = useState(null);
+  const [isModifying, setIsModifying] = useState(false);
+  const navigate = useNavigate();
 
-    const handleWritingButtonClick = () => {
-        setIsWriting(true);
-        setSelectedPostId(null); 
-    };
+  const handleWritingButtonClick = () => {
+    setIsWriting(true);
+    setSelectedPostId(null);
+  };
 
-    const handlePostClick = (postId) => {
-        setSelectedPostId(postId); // 선택된 게시물의 ID를 업데이트
-        setIsWriting(false);
-    };
+  const handlePostClick = (postId) => {
+    setSelectedPostId(postId); // 선택된 게시물의 ID를 업데이트
+    setIsWriting(false);
+  };
 
-    const updateIsModifying = (isModifying) => {
-      setIsModifying(isModifying);
+  const updateIsModifying = (isModifying) => {
+    setIsModifying(isModifying);
+  };
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (!accessToken) {
+      navigate("/login");
     }
+  }, [navigate]);
 
-    return (
-        <MainWrap>
-            <SideBar />
-            <PostMannager onWritingButtonClick={handleWritingButtonClick} onPostClick={handlePostClick} isModifying={isModifying} /> 
-            {!isWriting && selectedPostId !== null && <DetailsView postId={selectedPostId} updateIsModifying={updateIsModifying}/>} {/* 선택된 게시물의 ID가 있을 때만 DetailsView를 렌더링 */}
-            {isWriting && <Writing  />}
-        </MainWrap>
-    );
+  return (
+    <MainWrap>
+      <SideBar />
+      <PostMannager
+        onWritingButtonClick={handleWritingButtonClick}
+        onPostClick={handlePostClick}
+        isModifying={isModifying}
+      />
+      {!isWriting && selectedPostId !== null && (
+        <DetailsView
+          postId={selectedPostId}
+          updateIsModifying={updateIsModifying}
+        />
+      )}{" "}
+      {/* 선택된 게시물의 ID가 있을 때만 DetailsView를 렌더링 */}
+      {isWriting && <Writing />}
+    </MainWrap>
+  );
 }
 
 const MainWrap = styled.div`
